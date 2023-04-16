25d3aaf18c597a3ee023911e3d3158f51284c460
Iqboljon <iqboljon2003.yuldashev@gmail.com>
2023-04-16 23:21:02 +0500

    Data types

---
+++ main.js
@@ -0,0 +1,116 @@
+// variable => o'zgaruvchi
+// keyword => var , let , const
+// var name="Ali";
+// var name= "karim"; => qayta e'lon qilish
+// name="Sobir" => qiymat biriktirish
+// console.log(name);
+
+
+// # let
+
+// let name="Gani";
+// // let name="Karim";
+// name="Karim"
+// console.log(name);
+// # const
+// const age=19;
+// // const age=20;
+// age=20;
+// console.log(age);
+
+
+// Hoisting => tepaga ko'tarish
+
+
+// var age; /// => hoisting
+
+// console.log(age);
+// var age=19;
+// {
+//     console.log(age);
+
+
+//     if(age>20){
+//         console.log("salom");
+//     }
+
+//     // Temporal deadzone
+
+
+
+//     let age=20;
+// }
+// console.log(age);
+
+// console.log(age);
+// let age=20;
+
+
+// console.log(age);
+// const age=20;
+
+
+// Temporal deadzone
+
+
+
+///// Datatypes
+
+// primitive  and non-primitive;
+
+
+// primitive => Number,string,null,undefined,bigInt,symbol,Boolean;
+// non=primitive=> Object(object,array,function);
+
+
+// let num=255e+3;
+// console.log(num);
+
+// // let st="19";=> ' ' , ` `;
+// let st=`function(){}`
+// console.log(typeof st);
+
+// let age=null;
+// // age=age+14;
+// // console.log( typeof age); => typeof object
+
+
+// let un;
+// console.log(typeof un);
+
+
+// #boolean
+
+// let num=false;
+// console.log(5<4);
+// // console.log(typeof num);
+
+// #bigInt
+
+
+// let age=Number.MAX_SAFE_INTEGER;
+// console.log(age*2);
+// const x = BigInt(Number.MAX_SAFE_INTEGER) + 1n;
+// const y = BigInt(Number.MAX_SAFE_INTEGER) + 2n;
+// console.log(x===y);
+
+
+// #Symbol
+
+// let sym1=Symbol("text");
+// let sym2=Symbol("text");
+// console.log(sym1);
+// console.log(sym1===sym2);
+
+
+// let age=19;
+
+// let obj={
+//     name:"Ali",
+// }
+// console.log(ob j);
+
+
+
+
+
