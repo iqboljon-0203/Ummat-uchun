5b2d8878296282bcd2f5f92eea6230345212e22c
Iqboljon <iqboljon2003.yuldashev@gmail.com>
2023-04-29 23:50:59 +0500

    Added another lessons

---
+++ JS lesson 2/main.js
@@ -0,0 +1,71 @@
+///// String
+/////Data types => primitive and non-primitive
+// Number,string,null,undefined,boolean,symbol,BigInt=>primitive
+// Object(array,function,obeject)=> non-primitive 
+
+// let var const
+// var age=18;
+// // var age=20;
+// age=20
+
+// let const 
+
+
+// let age=20;
+// // let age=35;
+// age=25;
+// console.log(age);
+
+
+// const 
+
+// const age=20;
+// const age=25;
+// const age=17;
+// age=20;
+// console.log(age);
+
+
+// #String
+// // " ", ' ', ``
+// let str="Hello JS";
+// console.log( typeof str);
+
+// /// Boolean => true,false
+
+// // typeof 
+// let yes=true;
+// let no= false;
+// console.log(typeof yes);
+// console.log( typeof no);
+
+
+//// falsy => 0,"",null,undefined,-0,false
+//// truthy
+// let a=18;
+// let b=9;
+// let c=12;
+// if(a>b){
+//     console.log(a);
+// }else if(a>c){
+//     console.log(a);
+// }else if(c>a){
+//     console.log(c);
+// }else if(c>b){
+//     console.log(c);
+// }
+// else{
+//     console.log(b);
+// }
+// console.log("5"==="5");
+
+// 5<4?console.log("red"):5<3?console.log("red"):console.log("yellow");;
+let a=17;
+switch(a){
+    case 17:console.log("red");
+}
+
+
+
+
+
